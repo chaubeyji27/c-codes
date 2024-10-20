8e8391ce9fd2729167d3213ad56f91f4cc5ccb8c
chaubeyji27 <shivamyogeshchaubey27@gmail.com>
2024-10-20 22:09:22 +0530

    Add files via upload

---
+++ devshiv.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+   int l,b, area;
+   printf("enter the length and breadth of the rectangle");
+   scanf("%d%d", &l, &b);
+   area = l * b;
+   printf("the area of the rectangle is %d", area);
+    return 0;
+}
